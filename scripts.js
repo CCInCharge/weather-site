5ee50b398285a0a1af1edbe05e87c5ec34f3f9bc
CCInCharge <charles.l.chen.clc@gmail.com>
2016-12-21 16:56:48 -0800

    Moved Wunderground API call to within IP location call for async handling

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ $.get("https://ipinfo.io", function(response) {
 }, "jsonp")
 */
 
-function getLocation() {
+function getWeather() {
   $.getJSON('https://ipinfo.io', function(data){
     var city = data.city;
     var country = data.country;
@@ -20,29 +20,18 @@ function getLocation() {
     }
 
     $("#location").text(location_text);
+
+    queryURL = "https://api.wunderground.com/api/066cbf2575a1004c/forecast/conditions/q/" + loc[0] + ".json";
+    $.getJSON(queryURL, function(data) {
+      curData = data;
+      var temp_f = data.current_observation.temp_f;
+      var temp_c = data.current_observation.temp_c;
+      var weather = data.current_observation.weather;
+
+      $("#temperature").text(temp_f);
+      $("#current-weather").text(weather);
+    })
   });
 }
 
-/*
-function getWeather() {
-  var lat = loc[0];
-  var lon = loc[1];
-  $.getJSON('
-  var appid = "78a1ff6ef3713d277bd6d47f094fb816";
-}
-*/
-
-function getWeather() {
-  queryURL = "https://api.wunderground.com/api/066cbf2575a1004c/forecast/conditions/q/" + loc[0] + ".json";
-  $.getJSON(queryURL, function(data) {
-    curData = data;
-    var temp_f = data.current_observation.temp_f;
-    var temp_c = data.current_observation.temp_c;
-    var weather = data.current_observation.weather;
-
-    $("#temperature").text(temp_f);
-    $("#current-weather").text(weather);
-  })
-
-
-}
+getWeather();
