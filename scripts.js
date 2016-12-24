290bbf5aeabdd52bf7e23eed01aee78e07aa44ab
CCInCharge <charles.l.chen.clc@gmail.com>
2016-12-24 14:55:28 -0800

    Fixed geolocation functionality

--- scripts.js
+++ scripts.js
@@ -1,11 +1,5 @@
-/*
-$.get("https://ipinfo.io", function(response) {
-  console.log(response.ip, response.country);
-}, "jsonp")
-*/
 
-$(document).ready(function() {
-  function switchUnits() {
+function switchUnits() {
     if (document.getElementById("units").className == "degF") {
       $("#units").removeClass("degF");
       $("#units").addClass("degC");
@@ -17,49 +11,25 @@ $(document).ready(function() {
       $("#units").html("&deg;F");
       $("#temperature").html(temp_f);
     }
-  }
+}
 
-  function getLocation(callback) {
-    if (!navigator.geolocation) {
-      return;
-    }
-
+$(document).ready(function() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        loc = [position.coords.latitude.toString() + "," +  position.coords.longitude.toString()];
-        callback(loc);
-      })
+            navigator.geolocation.getCurrentPosition(function(position) {
+                lat = position.coords.latitude.toString();
+                lon = position.coords.longitude.toString();
+            $.getJSON("https://api.wunderground.com/api/066cbf2575a1004c/forecast/conditions/q/" + lat+ "," + lon + ".json", function(data) {
+ //     var curData = data;
+      temp_f = data.current_observation.temp_f;
+      temp_c = data.current_observation.temp_c;
+      city = data.current_observation.observation_location.full;
+      weather = data.current_observation.weather;
+
+      $("#current-city").text(city);
+      $("#temperature").text(temp_f);
+      $("#current-weather").text(weather);
+            })
+            })
     }
-  }
-
-  function parseData(data) {
-    temp_f = data.current_observation.temp_f;
-    temp_c = data.current_observation.temp_c;
-    var weather = data.current_observation.weather;
-
-    $("#temperature").text(temp_f);
-    $("#current-weather").text(weather);
-  }
-
-  function getWeather() {
-    $.getJSON(getLocation(function(loc){
-      return "https://api.wunderground.com/api/066cbf2575a1004c/forecast/conditions/q/" + loc[0] + ".json";}), parseData(data));
-  //    queryURL = "https://api.wunderground.com/api/066cbf2575a1004c/forecast/conditions/q/" + loc[0] + ".json";
-  /*
-      $.ajax({
-        url: getLocation(function(loc){
-          return "https://api.wunderground.com/api/066cbf2575a1004c/forecast/conditions/q/" + loc[0] + ".json";}),
-        dataType: 'json',
-        success: function(data) {
-        temp_f = data.current_observation.temp_f;
-        temp_c = data.current_observation.temp_c;
-        var weather = data.current_observation.weather;
-
-        $("#temperature").text(temp_f);
-        $("#current-weather").text(weather);
-      }})
-      */
-  }
-  getWeather();
-  // $(".switch-units").on("click",switchUnits());
 })
+
